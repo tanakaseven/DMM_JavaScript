e61122185fb837382c727901704ce094c64203b1
tanakaseven <162946544+tanakaseven@users.noreply.github.com>
2024-03-21 15:11:12 +0900

    課題提出_2回目

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 for (let i = 1; i <= 50; i++) {
   if (i % 10 === 0) {
     console.log(`今${i}回ループしました。`);
-  } else if (i % 4 === 0) {
+  } 
+  if (i % 4 === 0) {
     console.log(`4で割れる数です。${i}`);
   } else {
     console.log(i);
