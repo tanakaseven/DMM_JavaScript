0f9f0aa188d5c1e20373851edd8e393f5ac5c751
tanakaseven <162946544+tanakaseven@users.noreply.github.com>
2024-03-21 09:04:06 +0900

    課題提出

---
+++ app.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 50; i++) {
+  if (i % 10 === 0) {
+    console.log(`今${i}回ループしました。`);
+  } else if (i % 4 === 0) {
+    console.log(`4で割れる数です。${i}`);
+  } else {
+    console.log(i);
+  }
+}
+alert('50回カウントが終わりました。');
+
+
+
